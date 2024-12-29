5ad5c0b3715966a05a2ec245245c0a40675fc9b2
Ken Domino <kenneth.domino@domemtech.com>
2024-12-29 13:47:16 -0500

    Fix build.sh for mysql.

--- sql/mysql/Oracle/Antlr4ng/st.build.sh
+++ sql/mysql/Oracle/Antlr4ng/st.build.sh
@@ -1,7 +1,7 @@
-# Generated from trgen 0.23.7
+# Generated from trgen <version>
 set -e
 rm -rf node_modules package-lock.json
-npm install -g typescript ts-node
+#npm install -g typescript ts-node
 npm install
 
 if [ -f transformGrammar.py ]; then python3 transformGrammar.py ; fi
@@ -12,10 +12,18 @@ if [ -f transformGrammar.py ]; then python3 transformGrammar.py ; fi
 # parser and lexer.
 version=`grep antlr4 package.json | awk '{print $2}' | tr -d '"' | tr -d ',' | tr -d '\r' | tr -d '\n'`
 
-java -jar ./node_modules/antlr4ng-cli/*.jar -encoding utf-8 -Dlanguage=TypeScript   MySQLLexer.g4
-java -jar ./node_modules/antlr4ng-cli/*.jar -encoding utf-8 -Dlanguage=TypeScript   MySQLParser.g4
+<tool_grammar_tuples:{x |
+tsx --tsconfig ~/antlr-ng/tsconfig.json ~/antlr-ng/cli/runner.ts -Dlanguage=TypeScript <x.AntlrArgs> <antlr_tool_args:{y | <y> } > <x.GrammarFileName>
+} >
 
 rm -rf original
+
+tsc -p tsconfig.json --pretty
+exit 0
+
+
+java -jar ./node_modules/antlr4ng-cli/*.jar -encoding utf-8 -Dlanguage=TypeScript   MySQLLexer.g4
+java -jar ./node_modules/antlr4ng-cli/*.jar -encoding utf-8 -Dlanguage=TypeScript   MySQLParser.g4
 
 tsc -p tsconfig.json --pretty
 exit 0
