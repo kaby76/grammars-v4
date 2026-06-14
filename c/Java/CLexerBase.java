2005040446c8d415aaa2371043d9adc5a3b7cd5e
kaby76 <kenneth.domino@domemtech.com>
2026-06-14 14:21:36 -0400

    Remove debug output.

--- c/Java/CLexerBase.java
+++ c/Java/CLexerBase.java
@@ -76,7 +76,6 @@ public abstract class CLexerBase extends Lexer {
                 process.waitFor();
 
 		Files.writeString(Path.of(outputName), output);
-		System.err.println("YO" + output);
                 return CharStreams.fromString(output);
             } catch (Exception e) {
                 throw new RuntimeException("Failed to run gcc preprocessor", e);
